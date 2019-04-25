3063d9f29bca87a49f2fa6744c52c3102c78ff9c
Dat Nguyen <lonelyicevalley@icloud.com>
2019-04-25 21:12:29 +0700

    init project3

---
+++ robo1x/project3/LocalPlannerSixLink.m
@@ -0,0 +1,8 @@
+function out = LocalPlannerSixLink (x1, x2, obstacle)
+% Generate a random freespace configuration for the robot
+% This function checks if you can generate a straight line between x1 and 
+% x2 without collision. It return true when there is a valid line.
+
+
+
+end
---
+++ robo1x/project3/RandomSampleSixLink.m
@@ -0,0 +1,8 @@
+function x = RandomSampleSixLink (obstacle)
+% Generate a random freespace configuration for the robot
+% Hint: use CollisionCheck(fv, obstacle) to check if the configuration is
+% in freespace.
+
+
+
+end
